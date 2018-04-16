9a871c0e9bba71901caf9f1c811a76d3496e8556
ipolihas <ivpolihas@gmail.com>
2018-04-16 22:27:46 +0300

    hello Ivan

--- maven.team/src/main/java/lits/automation/java/at23/maven/team/EmployeeComparator.java
+++ maven.team/src/main/java/lits/automation/java/at23/maven/team/EmployeeComparator.java
@@ -9,6 +9,7 @@ public class EmployeeComparator implements Comparator<Employees>{
 		 String firstName1 = o1.getFName();
          String firstName2 = o2.getFName();
          return firstName1.compareTo(firstName2);
+         System.out.println("Hello Iryna!!!!");
 	}
 	
 
